27a4d6c5d605646c8650cfa2a7ef65d193343613
StarTif-V <tifanyviaa@gmail.com>
2025-07-01 01:19:20 -0500

    se agrego contenido de 7 sintomas para sintomas.js

---
+++ integradorweb/src/data/sintomas.js
@@ -0,0 +1,31 @@
+const sintomas = [
+{
+nombre: 'Dolor de cabeza',
+recomendados: ['paracetamol', 'ibuprofeno']
+},
+{
+nombre: 'Fiebre',
+recomendados: ['paracetamol', 'naproxeno']
+},
+{
+nombre: 'Congestión nasal',
+recomendados: ['loratadina', 'desloratadina']
+},
+{
+nombre: 'Dolor muscular',
+recomendados: ['ibuprofeno', 'naproxeno']
+},
+{
+nombre: 'Dolor de garganta',
+recomendados: ['paracetamol', 'ibuprofeno']
+},
+{
+nombre: 'Tos seca',
+recomendados: ['dextrometorfano', 'ambroxol']
+},
+{
+nombre: 'Náuseas',
+recomendados: ['metoclopramida', 'dimenhidrinato']
+}
+];
+export default sintomas;
